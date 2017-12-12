8d54dafa437f2add5ce7ea064f6d7d782f160a3f
Arun Babu N <ab@ameyem.com>
2017-12-12 18:10:36 +0530

    testcontroller changed

--- app/Http/Controllers/TestsController.php
+++ app/Http/Controllers/TestsController.php
@@ -94,11 +94,11 @@ class TestsController extends Controller
 
         $test2 = Test::find($test->id);
         $mydate = date_format($test2->created_at, 'd-M-Y H:i');
-        // $urls=Share::load('http://www.skills.ameyem.com/quiz', 'Wonna test your capabilities too!!!?',
-        // url('http://www.skills.ameyem.com/quiz/quickadmin/images/logo.png'))->services('facebook', 'gplus', 'twitter');
+        $urls=Share::load('http://www.skills.ameyem.com/quiz', 'Wonna test your capabilities too!!!?',
+        url('http://www.skills.ameyem.com/quiz/quickadmin/images/logo.png'))->services('facebook', 'gplus', 'twitter');
 
-        $urls=Share::load('http://localhost:8000/quiz', 'Wonna test your capabilities too!!!?',
-        url('http://localhost:8000/quiz/quickadmin/images/logo.png'))->services('facebook', 'gplus', 'twitter');
+        // $urls=Share::load('http://localhost:8000/quiz', 'Wonna test your capabilities too!!!?',
+        // url('http://localhost:8000/quiz/quickadmin/images/logo.png'))->services('facebook', 'gplus', 'twitter');
         // return redirect()->route('results.show', [$test->id]);
         // return redirect()->route('results.summary', [$test->id]);
         
