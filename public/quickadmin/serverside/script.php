c0befbfdb9196a92fee4bdd4b793494b51976b60
Arun Babu N <ab@ameyem.com>
2017-12-13 02:07:00 +0530

    script php changes

--- public/quickadmin/serverside/script.php
+++ public/quickadmin/serverside/script.php
@@ -10,14 +10,15 @@
 	$success = file_put_contents($file, $data);
 	print $success ? $file : 'Unable to save the file.';
 
-	define('HTML_DIR', '../../u/');
+	//define('HTML_DIR', '../../u/');
+	define('HTML_DIR', 'http://www.skills.ameyem.com/quiz/u/');
 	$htmlFile = HTML_DIR . $fileuid . '.html';; // or .php   
 	$fh = fopen($htmlFile, 'w'); // or die("error");  
 	$stringData = '
 	<html>
 	<head>
 		<meta property="og:title" content="AmeyemQuiz - how well do you know your subject? Test yourself" />
-		<meta property="og:image" content="../quickadmin/serverside/'.$file.'" />
+		<meta property="og:image" content="http://www.skills.ameyem.com/quiz/quickadmin/serverside/'.$file.'" />
 		<meta property="og:description" content="Ameyem Quiz. Powered by skills.ameyem.com" />
 		<meta property="og:url" content="http://www.skills.ameyem.com/quiz/u/'.$fileuid.'.html" />
 	</head>
